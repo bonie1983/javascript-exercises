2a9500ed717a5625e02e19f0fe9ce8bcb3cb8583
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2024-04-29 19:57:09 +0200

    02_repeatString.js completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    string = ""
 
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        for (let i = 0; i < num; i++) {
+            string = string + word
+        }
+        return string;
+    }
 };
+
+console.log(repeatString("hey", 3));
+
 
 // Do not edit below this line
 module.exports = repeatString;
