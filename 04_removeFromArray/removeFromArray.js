00b5b20480dcde4b920481782c792025ec8f6c75
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2024-05-01 20:42:45 +0200

    Tests from 04-removeFromArray 1-3-4 accomplished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+    
+    newArray = []
 
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+        if (remove == array[i]) {
+            delete array[i]
+        } else {
+            newArray.push(element)
+        }
+    }
+    return newArray;
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 3, 2))
 // Do not edit below this line
 module.exports = removeFromArray;
