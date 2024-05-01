657196037d6598d2df212dac0271dfe3a93eb027
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2024-05-01 19:16:12 +0200

    Exercise 03_reverseString accomplished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+const reverseString = function(frase) {
+    splitedWordArray = []
+    wordReversed = ""
 
+    for (let index = 0; index < frase.length; index++) {
+        splitedWordArray.push(frase[index]);
+    }
+    arrayReversed = splitedWordArray.reverse()
+
+    for (let i = 0; i < arrayReversed.length; i++) {
+        wordReversed = wordReversed + arrayReversed[i]
+    }
+
+    return wordReversed
+
+    
 };
 
+reverseString("Hola")
 // Do not edit below this line
 module.exports = reverseString;
