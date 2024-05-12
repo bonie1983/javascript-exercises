e04d48b0789acc0e498008057ae562e0e913ecc9
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2024-05-12 11:12:01 +0200

    05._sumAll 3 test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,23 @@
 const sumAll = function() {
     const array = arguments;
     let sum = 0;
-    for (let i = 1; i <= array[array.length - 1]; i++) {
-        sum++;
-        console.log(sum);
+    let counter = 0;
+
+    if (array[0] < array[array.length -1 ]) {
+        for (let i = 1; i <= array[array.length - 1]; i++) {
+            counter++;
+            sum = sum + counter;
+        }
+    } else if (array[0] > array[array.length -1 ]) {
+        for (let i = 1; i <= array[0]; i++) {
+            counter++;
+            sum = sum + counter;
+        }
     }
     return sum;
 };
 
-console.log(sumAll(1, 4));
-
+console.log(sumAll(4, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
