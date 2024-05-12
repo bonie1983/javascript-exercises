3504e7825f70b816b09487e9952dd667bf2011c1
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2024-05-12 12:25:11 +0200

    Exercise 05_sumAll tests completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,7 +3,9 @@ const sumAll = function() {
     let sum = 0;
     let counter = 0;
 
-    if (array[0] < array[array.length -1 ]) {
+    if ((array[0] < 0) || (array[array.length -1] < 0) || typeof array[array.length -1] === "string" || isNaN(array[array.length -1])) {
+        return "ERROR";
+    } else if (array[0] < array[array.length -1 ]) {
         for (let i = 1; i <= array[array.length - 1]; i++) {
             counter++;
             sum = sum + counter;
@@ -14,14 +16,12 @@ const sumAll = function() {
             sum = sum + counter;
         }
     }
+    console.log(typeof(array[array.length -1]));
     return sum;
+
 };
 
-console.log(sumAll(4, 1));
+console.log(sumAll(10, "90"));
 
 // Do not edit below this line
 module.exports = sumAll;
-
-
-
-// return array[array.length - 1];
