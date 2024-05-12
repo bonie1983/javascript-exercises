b21ace8735fc34fece7e748c79c4f83093358af5
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2024-05-12 10:42:33 +0200

    Very close to solve 05.Javascript exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
 const sumAll = function() {
-
+    const array = arguments;
+    let sum = 0;
+    for (let i = 1; i <= array[array.length - 1]; i++) {
+        sum++;
+        console.log(sum);
+    }
+    return sum;
 };
+
+console.log(sumAll(1, 4));
+
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+
+// return array[array.length - 1];
