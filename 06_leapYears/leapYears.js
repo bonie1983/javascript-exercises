976f08620889bee3f64d2baa2875386ddea8200b
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2024-05-15 18:38:59 +0200

    They're left 2 tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        return true
+    } else {
+        return false
+    }
 };
 
+
+console.log(1900 % 4)
+console.log(700 % 4)
 // Do not edit below this line
 module.exports = leapYears;
