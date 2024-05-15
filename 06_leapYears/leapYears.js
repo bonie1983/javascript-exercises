5b21e4ac90d736a6e38900303b281bbb1bf48de5
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2024-05-15 18:47:08 +0200

    exercise 06_leapYears done sucessfully

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
 const leapYears = function(year) {
-    if (year % 4 === 0) {
+    if ( year % 100 === 0 && year % 400 === 0) {
+        return true
+    } else if (year % 100 === 0) {
+        return false
+    } else if (year % 4 === 0) {
         return true
     } else {
         return false
@@ -11,3 +15,10 @@ console.log(1900 % 4)
 console.log(700 % 4)
 // Do not edit below this line
 module.exports = leapYears;
+
+
+/*if (year % 4 === 0) {
+    return true
+} else {
+    return false
+}*/
