b2ec030d19e3025826fab090d31b1efaf1386390
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2025-11-23 18:40:15 +0300

    Exercise 11 solved

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  farenheiht = Math.round(((f - 32) * 5 / 9) * 10) / 10;
+  farenheiht = parseFloat(farenheiht.toFixed(1));
+  return farenheiht;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  ceslius = Math.round(((c * 9 / 5) + 32) * 10) / 10;
+  celsius = parseFloat(ceslius.toFixed(1));
+  return celsius;
 };
+
+
 
 // Do not edit below this line
 module.exports = {
