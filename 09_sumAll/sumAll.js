0a17df2c01f2fe31358227eea13cfe05168e6e5e
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2025-11-23 17:54:07 +0300

    exercise 9 solved

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0) return 'ERROR';
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return 'ERROR';
+    
+    const start = Math.min(x, y);
+    const end = Math.max(x, y);
+    let sum = 0;
 
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
