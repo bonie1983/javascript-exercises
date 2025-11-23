1c4754aadf2054e5ce70a5f73db9db01e23aab9e
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2025-11-23 17:46:51 +0300

    Exercise 8 solver

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(lista, ...args) {
+    const nuevoArray = []; 
+
+    for (const item of lista) {
+        if (!args.includes(item)) {
+            nuevoArray.push(item);
+        }
+    }
+    
+    return nuevoArray;
 };
 
 // Do not edit below this line
