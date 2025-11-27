7fe631c7f244f6e9b02137b6c7e27e306b844a3c
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2025-11-27 10:06:23 +0300

    exercise 12 solved

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
-	
+const add = function(x , y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x , y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+  let total = 0;
+	for (let i = 0; i < x.length; i++) {
+    total += x[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+  let total = x[0];
+  for (let i = 1; i < x.length; i++) {
+    total *= x[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x , y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let listNumbers = [];
+  for (let i = 1; i <= x; i++) {
+    listNumbers.push(i);
+  }
+  if (x === 0) {
+    return 1;
+  } else {
+    return multiply(listNumbers);
+  }
 };
+console.log();
 
 // Do not edit below this line
 module.exports = {
