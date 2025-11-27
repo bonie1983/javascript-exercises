34f86b13cb747941fb5ede14c20a4a7bae088977
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2025-11-27 13:27:43 +0300

    Exercise 16 solved

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,40 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) { 
+    
+    const obtenerEdad = (person) => {
+        const añoDeReferencia = person.yearOfDeath || new Date().getFullYear(); 
+        
+        return añoDeReferencia - person.yearOfBirth;
+    };
+    
+    return persons.reduce((personaMasViejaHastaAhora, personaActual) => {
+        
+        const edadMasVieja = obtenerEdad(personaMasViejaHastaAhora);
+        const edadActual = obtenerEdad(personaActual);
 
+        if (edadActual > edadMasVieja) {
+            return personaActual;
+        } else {
+            return personaMasViejaHastaAhora;
+        }
+    }, persons[0]);
 };
 
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
 // Do not edit below this line
 module.exports = findTheOldest;
