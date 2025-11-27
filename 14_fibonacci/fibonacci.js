83bc275f8e363cad2a7a8f477cba604df2e7e2be
Steve Rozada <30368272+bonie1983@users.noreply.github.com>
2025-11-27 11:10:12 +0300

    Exercise 14 solved

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    let n = Number(x);
+    if (isNaN(n) || n < 0) return "OOPS";
+    if (n === 0) return 0;
+    if (n === 1) return 1;
 
+    let a = 0
+    let b = 1
+    let temp;
+    for (let i = 2; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
